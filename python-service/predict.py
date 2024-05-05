07c1ed4a4aaaa728c6275eb0343071640b4f474b
enesyukselx <enesyuksel9903@gmail.com>
2024-05-05 21:37:39 +0300

    refactor: move prob_viz function to separate file

--- python-service/predict.py
+++ python-service/predict.py
@@ -6,19 +6,11 @@ from utils.mediapipe_detection import mediapipe_detection as mpdetection
 from utils.extract_keypoints import extract_keypoints
 from utils.draw_styled_landmarks import draw_styled_landmarks as dslandmarks
 from utils.base64_to_image import base64_to_image
+from utils.prob_viz import prob_viz, colors
 from settings.create_model import actions
 from data.sampleBase64 import base64Image
 
 model = load_model('sign_language.keras')
-
-colors = [(245,117,16), (117,245,16), (16,117,245)]
-def prob_viz(res, actions, input_frame, colors):
-    output_frame = input_frame.copy()
-    for num, prob in enumerate(res):
-        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
-        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-        
-    return output_frame
 
 #image_url = "https://www.shutterstock.com/image-photo/old-man-making-out-hands-260nw-551186926.jpg"
 #image = url_to_image(image_url)
