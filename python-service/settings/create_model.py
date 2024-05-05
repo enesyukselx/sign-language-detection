fd94996d6554c8c31f3b07278c4fe829fe018758
enesyukselx <enesyuksel9903@gmail.com>
2024-05-05 05:26:49 +0300

    train.py

--- python-service/settings/create_model.py
+++ python-service/settings/create_model.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 DATA_PATH="MP_Data"
 existing_label_map = {'merhaba':1,'tamam':2,'seni seviyorum':3}
 new_actions_with_labels = {}
@@ -5,3 +7,5 @@ new_actions_with_labels = {}
 no_sequences = 5
 # Videos are going to be 30 frames in length
 sequence_length = 30
+
+actions = np.array(list(existing_label_map.keys()))
